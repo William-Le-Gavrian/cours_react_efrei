b9d73ceed51beb5dfd7ba07ef0754a8f100385ee
Lévy ZIRE <levy.zire.ext@francetv.fr>
2024-11-20 15:01:38 +0100

    add bootsrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from "react-router-dom";
 import Welcome from "./pages/Welcome.jsx";
 import Home from "./pages/Home.jsx";
 import BookDetails from "./pages/BookDetails.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
     <>
-        <Routes>
-            <Route  path="/" element={<Home />}/>
-            <Route  path="/welcome" element={<Welcome />}/>
-            <Route path="/books/:id" element={<BookDetails />} />
-        </Routes>
-
+        <div className="container-fluid vh-100">
+            <div className="row">
+                <Header />
+            </div>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/welcome" element={<Welcome/>}/>
+                <Route path="/books/:id" element={<BookDetails/>}/>
+            </Routes>
+        </div>
     </>
   )
 }
